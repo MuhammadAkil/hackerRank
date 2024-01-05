b6a07bbf73fea2e9a33f93e75e74eb8ccb91e917
Akil <akil.siddique@postex.pk>
2024-01-05 17:24:33 +0500

    add two numbers

--- main.js
+++ main.js
@@ -1,1 +1,51 @@
-alert("Hello HackerRanks")
+function birthdayCakeCandles(candles) {
+  
+        // let max = candles.reduce((acc,current)=>Math.max(acc,current),0);
+      // let tallest=0;
+      // let  max=0;
+      //   for(let item of candles ){
+      //     if(item > max){
+      //       max=item;
+      //       document.write(max)
+      //     tallest=1;
+      //     }
+      //     else if(item===max){
+      //       tallest++;
+      //     }
+      //     }
+      //     document.write(tallest)
+      
+      let max=0;
+      let tallest=0;
+      for(let i of candles){
+        if(i>max){
+          max=i;
+          tallest=1
+        }else if(i===max){
+          tallest++;
+          
+        }
+      }
+        
+        document.write(tallest);
+      
+        
+        // sameNum= candles.filter((item)=>max==item);
+        // document.write(sameNum.length);
+}
+
+let candles = [4, 4,1,3,4,4,5,7,7,7,5,6,7,'7'];
+birthdayCakeCandles(candles);
+
+
+
+function solveMeFirst(a, b) {
+  // Hint: Type return a+b below 
+  let sum = 0;
+  sum=a+b; 
+  console.log(sum);
+  return sum;
+  
+}
+solveMeFirst(2, 3);
+
